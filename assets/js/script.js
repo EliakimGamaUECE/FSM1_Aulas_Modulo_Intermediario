369abc2865796163576b394c66cbdd5231858219
EliakimGama <eliakimgama@hotmail.com>
2024-12-02 10:10:01 -0300

    Aula 01

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+// strings - concatenação
+var a = "Eliakim "
+var b = " Gama "
+var c = "do Nascimento"
+var d = a + b + c
+
+// numeros-operação
+var idade = 33
+
+
+// mostrar
+alert("Seja bem-vindo!" + d +  " você tem " + idade + "anos")
+document.write(`Seja bem-vindo! ${d} você tem ${idade} anos`)
