e2ba9f68755dd132a520bd96a41c7757e05e490f
EliakimGama <eliakimgama@hotmail.com>
2024-12-06 17:47:59 -0300

    Aula 02

--- Aula_02/assets/js/script.js
+++ Aula_02/assets/js/script.js
@@ -7,8 +7,11 @@ var d = a + b + c
 
 // numeros-operação
 var idade = 33
+var valor = false
 
 
 // mostrar
-alert("Seja bem-vindo!" + d +  " você tem " + idade + "anos")
-document.write(`Seja bem-vindo! ${d} você tem ${idade} anos`)
+// alert("Seja bem-vindo!" + d +  " você tem " + idade + "anos")
+// document.write(`Seja bem-vindo! ${d} você tem ${idade} anos`)
+
+document.write(typeof(valor))
