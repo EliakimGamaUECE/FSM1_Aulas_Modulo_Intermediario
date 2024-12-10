084dba01ca0f71bb02ff00227ec9e3828e22bd00
EliakimGama <eliakimgama@hotmail.com>
2024-12-10 07:53:48 -0300

    Aula 03

---
+++ Aula_03/assets/js/script.js
@@ -0,0 +1,38 @@
+
+// strings - concatenação
+var a = "Eliakim "
+var b = " Gama "
+var c = "do Nascimento"
+var d = a + b + c
+
+// numeros-operação
+var idade = 33
+var valor = false
+
+
+// mostrar
+// alert("Seja bem-vindo!" + d +  " você tem " + idade + "anos")
+// document.write(`Seja bem-vindo! ${d} você tem ${idade} anos`)
+
+var valor1 = 5
+var valor2 = 3
+
+
+var n2 = 11
+
+// if(5>3){
+
+//     soma = n1 + n2
+//     document.write(soma)
+// }
+// else{
+//     document.write(" Não somei pq a condição é falsa")
+// }
+
+
+for(var n1=0;n1<1000;n1++){
+    document.write(n1 + "<br>")
+}
+
+
+
