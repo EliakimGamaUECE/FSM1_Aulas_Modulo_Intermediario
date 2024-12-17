8a5578483209249fca6a10e1f5c26d32db10c4a2
EliakimGama <eliakimgama@hotmail.com>
2024-12-17 09:39:14 -0300

    Aula 05

---
+++ Aula_05/assets/js/script.js
@@ -0,0 +1,75 @@
+
+// strings - concatenação
+// var a = "Eliakim "
+// var b = " Gama "
+// var c = "do Nascimento"
+// var d = a + b + c
+
+// numeros-operação
+// var idade = 33
+// var valor = false
+
+
+// mostrar
+// alert("Seja bem-vindo!" + d +  " você tem " + idade + "anos")
+// document.write(`Seja bem-vindo! ${d} você tem ${idade} anos`)
+
+// var valor1 = 5
+// var valor2 = 3
+
+
+
+
+// if(5>3){
+
+//     soma = n1 + n2
+//     document.write(soma)
+// }
+// else{
+//     document.write(" Não somei pq a condição é falsa")
+// }
+
+
+
+
+// var n2 = 30
+// let n3 = 40
+
+
+
+// for(let n1 = 0;n1<=10;n1++){
+//     document.write(n1 + "<br>")
+// }
+
+// n1 = n1 + 1
+// document.write(n1 + "<br>")
+
+
+// function somar (x,y) {
+
+//     return x + y
+// }
+
+// let resultado = somar(55,89)
+
+// document.write(resultado)
+
+
+// let resultado1 = somar(10,80)
+// document.write(resultado1)
+
+let pessoa = [
+{
+    nome:"Eliakim",
+    idade:33,
+    lugarPreferido:"Praia"
+},
+{
+    nome:"Caleb",
+    idade:8,
+    lugarPreferido:"Serra"
+}
+]
+
+console.table(pessoa.idade)
+
